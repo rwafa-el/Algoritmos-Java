213d203ba2f5d71065e603bc40e3b99598bbd026
Rafael Luiz Amazone Lopes <rafaeluiztwia@outlook.com>
2026-06-02 11:45:37 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,37 @@
-public class Main {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Math;
+
+public class Main{
     public static void main(String[] args){
-        Bicicleta c1 = new Bicicleta();
-        c1.vmax(80);
-        c1.acelerar(100);
-        c1.mudarMarcha(10);
-        c1.imprimirEstados();
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        ArrayList<Double> amostras = new ArrayList<Double>();
+        System.out.println("Digite um número de amostras:");
+        int a = scan.nextInt();
         
-        Bicicleta c2 = new Bicicleta();
-        c2.vmax(100);
-        c2.acelerar(80);
-        c2.acelerar(30);
-        c2.mudarMarcha(7);
-        c2.imprimirEstados();
+        for(int i = 0; i < a; i++){
+            double b = rand.nextDouble(50);
+            amostras.add(b);
+        }
         
-        c1.freiar(50);
-        c1.imprimirEstados();
+        double soma = 0;
+        for(int i = 0; i < amostras.size(); i++){
+            soma = soma + amostras.get(i);
+        }
+        double media = soma/amostras.size();
+        System.out.println("Média: " + media);
+    
+        
+    
+        double desvio = 0;
+        for(int i = 0; i < amostras.size(); i++){
+            desvio = desvio + Math.pow((amostras.get(i) - media),2);
+        }
+        desvio = Math.sqrt(desvio/amostras.size());
+        
+        System.out.println("Desvio: " + desvio);
         
     }
 }
